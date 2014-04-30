11d959dd88195230ad4ad56d652b8e082b6975f8
totlth <totlth1@gmail.com>
2014-04-29 22:43:36 -0400

    first commit

---
+++ cm.mk
@@ -0,0 +1,18 @@
+## Specify phone tech before including full_phone
+$(call inherit-product, vendor/cm/config/gsm.mk)
+
+# Release name
+PRODUCT_RELEASE_NAME := fx3mt
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/lge/fx3mt/device_fx3mt.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := fx3mt
+PRODUCT_NAME := cm_fx3mt
+PRODUCT_BRAND := lge
+PRODUCT_MODEL := fx3mt
+PRODUCT_MANUFACTURER := lge
---
+++ device_fx3mt.mk
@@ -0,0 +1,24 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# The gps config appropriate for this device
+$(call inherit-product, device/common/gps/gps_us_supl.mk)
+
+$(call inherit-product-if-exists, vendor/lge/fx3mt/fx3mt-vendor.mk)
+
+DEVICE_PACKAGE_OVERLAYS += device/lge/fx3mt/overlay
+
+LOCAL_PATH := device/lge/fx3mt
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+	LOCAL_KERNEL := $(LOCAL_PATH)/kernel
+else
+	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
+endif
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_KERNEL):kernel
+
+$(call inherit-product, build/target/product/full.mk)
+
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
+PRODUCT_NAME := full_fx3mt
+PRODUCT_DEVICE := fx3mt
